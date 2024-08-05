e98f5a7d83b8121168a05d5f8618956dec0c1296
Akash Das <akash.raj180@gmail.com>
2024-08-05 20:42:15 +0530

    Initial Commit

---
+++ Arrays/FindMaxMin.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class FindMaxMin {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,1,20,35,12};
+        int min=arr[0];
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min)
+                min=arr[i];
+            if(arr[i]>max)
+                max=arr[i];
+        }
+        System.out.println("Max : "+max+" , Min :"+min);
+    }
+}
